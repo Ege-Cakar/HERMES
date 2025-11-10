53971b548269799b5ad56aa4250609d7dd5d95f1
Jasmine Andresol <jasmineandresol@Jasmines-Air.lan>
2025-11-10 11:02:35 -0500

    generate_lean_proofs TODO

--- scripts/create_pairs.py
+++ scripts/create_pairs.py
@@ -1,2 +1,1 @@
-{"id":"ex_0001","nl_text":"Every bounded monotone sequence converges.","lean_text":"import Mathlib\n-- theorem statement + proof ..."}
-{"id":"ex_0002","nl_text":"If a function is differentiable then it is continuous.","lean_text":"import Mathlib\n-- theorem statement + proof ..."}
+# TODO
